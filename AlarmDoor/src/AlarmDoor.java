0d848c1391e91cffeecaf670ba2e692591217f84
Phil Huang <pichuang@cs.nctu.edu.tw>
2016-01-13 18:03:12 +0800

    AlarmDoor

---
+++ AlarmDoor/src/AlarmDoor.java
@@ -0,0 +1,20 @@
+/**
+ * Created by roan on 2016/1/13.
+ */
+public class AlarmDoor extends Door implements Alarm{
+
+    @Override
+    public void alarm() {
+
+    }
+
+    @Override
+    void open() {
+
+    }
+
+    @Override
+    void close() {
+
+    }
+}
---
+++ AlarmDoor/src/Door.java
@@ -0,0 +1,9 @@
+/**
+ * Created by roan on 2016/1/13.
+ */
+abstract class Door {
+    // is-a
+    abstract void open();
+
+    abstract void close();
+}
